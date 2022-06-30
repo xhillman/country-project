d7b928bdbd374fc4176a46581c36f395387d35e7
Xavier Hillman <xhillman13@gmail.com>
2022-06-30 19:15:13 -0400

    added photo interactivity and prompt verification

--- app.js
+++ app.js
@@ -14,22 +14,44 @@ function tryAgain() {
     askYears();
 };
 
-function makeSuggestion() {
-    let text = "Which do your prefer?\nPress OK for Hiking or Cancel for Snorkeling"
-    if (confirm(text) == true) {
-        document.write("Next time you're in Iceland, try to find the trolls!");
-    } else {
-        document.write("Indonesia has a bunch of temples too! Check them out!");
+// function makeSuggestion() {
+//     let text = "Which do your prefer?\nPress OK for Hiking or Cancel for Snorkeling"
+//     if (confirm(text) == true) {
+//         document.write("Next time you're in Iceland, try to find the trolls!");
+//     } else {
+//         document.write("Indonesia has a bunch of temples too! Check them out!");
+//     }
+// };
+
+function chooseCountry() {
+    userCountry = prompt("Iceland or Indonesia?");
+    if (userCountry.toLowerCase() == "iceland") {
+        document.write("Iceland gets");
+    }   else if (userCountry.toLowerCase() == "indonesia") {
+        document.write("Indonesia gets")
+    }   else {
+        tryAnother();
     }
 };
 
-function chooseCountry() {
-    let userCountry = prompt("Iceland or Indonesia?");
-    if (userCountry.toLowerCase() == "iceland") {
-        document.write("<<< You chose Iceland!");
-    }   else if (userCountry.toLowerCase() == "indonesia") {
-        document.write("You chose Indonesia >>>");
-    }   else {
-        document.write("Where are you headed?");
-    }
+function tryAnother() {
+    confirm("That's not one of the options! Let's try this again.")
+    chooseCountry();
+}
+
+function rateCountry() {
+    userRating = prompt("How would you rate it from 1 - 5?");
+    if (userRating > 5 || userRating < 1) {
+        notAnOption();
+    } else {
+        for (let i = 0; i < userRating; i++) {
+            document.write("<img src='img/clipart1753277.png' style='width:50px'>");
+        };
+    document.write("out of 5!");
+    };
 };
+
+function notAnOption() {
+    confirm("Maybe you didn't understand the question. Give me a real answer!");
+    rateCountry();
+}
